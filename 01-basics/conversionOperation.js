276e0be330d59ce9b75b952626c0f78e34bb066a
Gagan Shrivastav <gaganshrivastav111@gmail.com>
2026-03-13 16:14:38 +0000

    conversion of operator

--- 01-basics/conversionOperation.js
+++ 01-basics/conversionOperation.js
@@ -16,17 +16,59 @@
 // let booleanisLoggedIn = BooleanisLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let isLoggedIn = 0
+// let isLoggedIn = 0
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1 => true 0 => false 
-// "" => false
-// "gagan" =>true
+// // 1 => true 0 => false 
+// // "" => false
+// // "gagan" =>true
 
 
-let score = 33
+// let score = 33
 
-let newScore = String(score)
-console.log( typeof newScore)
+// let newScore = String(score)
+// console.log( typeof newScore)
+
+// *****************************Operations*********************************
+
+let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+// let str1 = "Hello"
+// let str2 = "Gagan"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log('1'+ 2);
+// console.log(1+ '2');
+// console.log('1'+ 2 + 2);
+// console.log(1+ 2 + '2');
+// console.log(1+ '2'+ 2);
+
+// console.log((3+4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3 
+// console.log(num1 = num2 = num3 = 2+2)
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);

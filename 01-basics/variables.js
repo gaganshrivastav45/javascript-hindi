8ef86b91f3450093e4a832e9ac4878307b4bfae7
Gagan Shrivastav <gaganshrivastav111@gmail.com>
2026-03-13 14:21:00 +0000

    variables

---
+++ 01-basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 145236;
+let accountEmail = "gagan123@gmail.com";
+var accounPassword = 123456;
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 122456
+accountEmail = "priyanshu@gmail.com"
+accounPassword=13562
+accountCity ="Delhi"
+
+console.table([accountId, accountEmail, accounPassword, accountCity, accountState])

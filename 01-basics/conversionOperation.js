54d06d717468758b30d7b866b752bd518f489286
Gagan Shrivastav <gaganshrivastav111@gmail.com>
2026-03-13 15:45:14 +0000

    conversion of datatypes

---
+++ 01-basics/conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = 'abc33'
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" = 33
+// "33abc" => NaN 
+// true => 1; false => 0 
+
+// isLoggedIn = 1
+
+// let booleanisLoggedIn = BooleanisLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true 0 => false 
+// "" => false
+// "gagan" =>true
+
+
+let score = 33
+
+let newScore = String(score)
+console.log( typeof newScore)

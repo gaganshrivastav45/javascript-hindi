64a2fd6bfcbd526644251413199acdc82093e510
Gagan Shrivastav <gaganshrivastav111@gmail.com>
2026-03-13 14:21:54 +0000

    datatypes

---
+++ 01-basics/dataTypes.js
@@ -0,0 +1,26 @@
+// "use strict"; //treat all js code as newer version
+
+// alert ( 3 + 3) //we are using node.js, not browser
+
+// console.log(3
+//     +
+    // 3); //code readability should be high 
+
+
+// number => 2 to power 53
+// bigint  
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefind => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "hitesh ")
+console.log(typeof 16 )
+console.log(typeof null ) // object
+console.log(typeof undefined ) //undefind
+
+

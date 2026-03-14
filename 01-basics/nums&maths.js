6a2306aaa6753d8047d2af206ab47751f576afad
Gagan Shrivastav <gaganshrivastav111@gmail.com>
2026-03-14 17:08:38 +0000

    comment all

--- 01-basics/nums&maths.js
+++ 01-basics/nums&maths.js
@@ -8,77 +8,77 @@
 // console.log(balance.toString().length);
 // console.log(balance.toFixed(2));
 
-// const otheerNumber = 123.8966
-// console.log(otheerNumber.toPrecision(3));
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));
 
-const hundreds =10000000
-console.log(hundreds.toLocaleString('en-IN'));
+// const hundreds =10000000
+// console.log(hundreds.toLocaleString('en-IN'));
 
-let num = "50.78"
-console.log(parseInt(num)) // decimal ko int mei badal deta hai 
+// let num = "50.78"
+// console.log(parseInt(num)) // decimal ko int mei badal deta hai 
 
-let num = "50.78"
-console.log(parseFloat(num)) //string ko decimal mei badal deta hai 
+// let num = "50.78"
+// console.log(parseFloat(num)) //string ko decimal mei badal deta hai 
 
-console.log(Number.MAX_VALUE)
+// console.log(Number.MAX_VALUE)
 
-console.log(Number.isInteger(10))
-console.log(Number.isInteger(10.5)) // check karta hai k number int hai ya nahi
+// console.log(Number.isInteger(10))
+// console.log(Number.isInteger(10.5)) // check karta hai k number int hai ya nahi
 
-console.log(isNaN("hello"))
-console.log(isNaN(100))  //check kata hai value number nahi hai
+// console.log(isNaN("hello"))
+// console.log(isNaN(100))  //check kata hai value number nahi hai
 
-let num = new Number(200)
-console.log(num.valueOf()) // number ki actual value return karta hai 
+// let num = new Number(200)
+// console.log(num.valueOf()) // number ki actual value return karta hai 
 
-console.log(Number.isFinite(100))
-console.log(Number.isFinite(Infinity)) // check karta hai ki number finite hai ya infinity
+// console.log(Number.isFinite(100))
+// console.log(Number.isFinite(Infinity)) // check karta hai ki number finite hai ya infinity
 
-console.log(Number.isSafeInteger(100)) // check karta hai ki number safe integer range mei hai ya nahi 
+// console.log(Number.isSafeInteger(100)) // check karta hai ki number safe integer range mei hai ya nahi 
 
-console.log(Number.EPSILON) // ye very small number hota hai jo floating point calculations mei use hota hai 
+// console.log(Number.EPSILON) // ye very small number hota hai jo floating point calculations mei use hota hai 
 
-console.log(Number.POSITIVE_INFINITY)
-console.log(Number.NEGATIVE_INFINITY)
+// console.log(Number.POSITIVE_INFINITY)
+// console.log(Number.NEGATIVE_INFINITY)
 
-let num = 1e6
-console.log(num)// large number kos hort form mei likh sakte hai 
+// let num = 1e6
+// console.log(num)// large number ko short form mei likh sakte hai 
 
-// ****************************************************Maths**********************************************
+// // ****************************************************Maths**********************************************
 
 
-let num = -10
-console.log(Math.abs(num)) // negaative number ko positive bnata hai 
+// let num = -10
+// console.log(Math.abs(num)) // negaative number ko positive bnata hai 
 
-console.log(Math.round(4.6))
-console.log(Math.round(4.2)) // numbers ko nearest integer mei round kart hai 
+// console.log(Math.round(4.6))
+// console.log(Math.round(4.2)) // numbers ko nearest integer mei round kart hai 
 
-console.log(Math.ceil(4.1)) // hamesha next integer deta hai 
+// console.log(Math.ceil(4.1)) // hamesha next integer deta hai 
 
-console.log(Math.floor(4.9)) //hamesha lower integer deta hai  
+// console.log(Math.floor(4.9)) //hamesha lower integer deta hai  
 
-console.log(Math.random()) // 0 or 1 ke beech random number deta hai 
+// console.log(Math.random()) // 0 or 1 ke beech random number deta hai 
 
-let num = Math.floor(Math.random() * 10) + 1
-console.log(num) // 1 se 10 ke beech mei random number deta hai 
+// let num = Math.floor(Math.random() * 10) + 1
+// console.log(num) // 1 se 10 ke beech mei random number deta hai 
 
-console.log(Math.max(10, 20, 30, 40)) // largest number find karta hai
+// console.log(Math.max(10, 20, 30, 40)) // largest number find karta hai
 
-console.log(Math.min(10, 20, 30, 40)) // smallest number find karta hai 
+// console.log(Math.min(10, 20, 30, 40)) // smallest number find karta hai 
 
-console.log(Math.pow(2,3)) // power calculate karta hai 
+// console.log(Math.pow(2,3)) // power calculate karta hai 
 
-console.log(Math.sqrt(64)) // square reet nikalta hai 
+// console.log(Math.sqrt(64)) // square root nikalta hai 
 
-console.log(Math.trunc(4.9))
-console.log(Math.trunc(4.2)) //Decimal ka integer part deta hai (decimal hata deta hai)
+// console.log(Math.trunc(4.9))
+// console.log(Math.trunc(4.2)) //Decimal ka integer part deta hai (decimal hata deta hai)
 
-console.log(Math.sign(10))
-console.log(Math.sign(-10))
-console.log(Math.sign(0)) // number positive hai ya negative btata hai
+// console.log(Math.sign(10))
+// console.log(Math.sign(-10))
+// console.log(Math.sign(0)) // number positive hai ya negative btata hai
 
-console.log(Math.cbrt(27)) // Cube root nikalta hai 
+// console.log(Math.cbrt(27)) // Cube root nikalta hai 
 
-console.log(Math.hypot(3,4))// multiple numbers ka square root of sum os squre nikalta hai 
+// console.log(Math.hypot(3,4))// multiple numbers ka square root of sum of squre nikalta hai 
 
-console.log(Math.PI) // pi ki value
+// console.log(Math.PI) // pi ki value

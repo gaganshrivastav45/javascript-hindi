9ecd91368492c87f29cbfa19e6925592a512695d
Gagan Shrivastav <gaganshrivastav111@gmail.com>
2026-03-14 15:12:19 +0000

    strings in javascript

---
+++ 01-basics/string.js
@@ -0,0 +1,23 @@
+// const name = "Gagan"
+// const count = 20
+// // console.log(name + count + ' coins');
+// console.log(`Hello my name is ${name} and i have ${count} coins `)
+
+// const realName = new String('Gaganshrivastav')
+
+// console.log(realName[0]);
+// console.log(realName.__proto__);
+// console.log(realName.length);
+// console.log(realName.toUpperCase());
+// console.log(realName.toLowerCase())
+// console.log(realName.charAt(7)); //string ke kisi index ka character btata hai
+// console.log(realName.indexOf("v")); //word kis index se start hua hai 
+// console.log(realName.trim()); //start or end ki sari spaces hata deta hai 
+// console.log(realName.includes("shrivastav")); //check karta hai ki word string mei hai ya nahi
+// console.log(realName.slice(0,5)); // string ka kuch hissa nikalta hai 
+// console.log(realName.replace("Gagan","Rahul")); //string mei kisi word ko dusre word mei badal deta hai
+// console.log(realName.substring(0,5)); //string ka ek hissa nikal deta hai 
+// console.log(realName.startsWith("Gagan")); //string kis word se shuru ho rahi hai 
+// console.log(realName.endsWith("stav")); //string kis word par khatam ho rahai hai 
+// console.log(realName.repeat(3)); //string ko kai baar repeat karta hai 
+// console.log(realName.split("-")) //string ko arrry mei badal deta hai 

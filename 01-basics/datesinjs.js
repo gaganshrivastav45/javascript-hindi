87336843bbbb772fb85aaa4b391e66bc58b9b3c2
Gagan Shrivastav <gaganshrivastav111@gmail.com>
2026-03-15 15:11:25 +0000

    dates

---
+++ 01-basics/datesinjs.js
@@ -0,0 +1,32 @@
+// let myDate = new Date()
+// console.log(myDate); // current date or time deta hai
+
+// let date = new Date()
+// console.log(date.toString()) //readable date formate deta hai 
+
+// let date = new Date()
+// console.log(date.toDateString()) // sirf date dikhata hai time nahi 
+
+// let date = new Date()
+// console.log(date.toLocaleString()) // local formate me date or time deta hai
+
+// let date = new Date()
+// console.log(date.toLocaleDateString()) // sirf date deta hai local format mei 
+
+// let date = new Date()
+// console.log(date.toLocaleTimeString()) // sirf time deta hai local format mei 
+
+// let date = new Date()
+// console.log(date.getFullYear()) //sirf year btata hai 
+
+// let date = new Date()
+// console.log(date.getMonth()) // current month batata hai
+
+// let date = new Date()
+// console.log(date.getDate()) // current date btata hai 
+
+// date ka typeof object hota hai  
+
+// Example:
+let myCreatedDate = new Date(2023, 0, 23)
+console.log(myCreatedDate.toString())

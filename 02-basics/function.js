df9bc79f3cf2e91fe5ff7a06436cf23520ca93d2
Gagan Shrivastav <gaganshrivastav111@gmail.com>
2026-03-19 13:39:59 +0000

    function in depth

---
+++ 02-basics/function.js
@@ -0,0 +1,60 @@
+// function sayMyName(){
+//   console.log("G")
+//   console.log("A")
+//   console.log("G")
+//   console.log("A")
+//   console.log("N")
+// }
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//   console.log(number1 + number2)
+// }
+
+// function addTwoNumbers(number1, number2){
+//   // let result = number1 + number2
+//   // return result 
+//   return number1 + number2 
+// }
+
+// // addTwoNumbers(30,20)
+//  const result = addTwoNumbers(30,20)
+//  console.log("Result:", result);
+
+// function loginUserMessage(username = "Priyanshu"){
+//   return `${username} just logged in`
+// }
+
+// console.log(loginUserMessage());
+
+// function calculateCartPrice(...num1){
+//   return num1
+// }
+
+// console.log(calculateCartPrice(200,400,500))
+
+// const user = {
+//   username: "Gagan",
+//   price: 199
+// }
+
+// function handleObject(anyObject){
+//   console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+// }
+
+// // handleObject(user)
+// handleObject({
+//   username: "sam",
+//   price: 399
+// });
+
+
+// const myNewArray = [200,300,500]
+
+// function returnSecondValue(getArray){
+//    return getArray[4]
+// }
+
+// // console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200,400,500,1000,1200]))

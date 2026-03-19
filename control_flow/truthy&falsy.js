435ef4d258010b0ceb3d09061fe0b84a32ca63a3
Gagan Shrivastav <gaganshrivastav111@gmail.com>
2026-03-19 17:24:56 +0000

    with ternary and nullish operator

---
+++ control_flow/truthy&falsy.js
@@ -0,0 +1,30 @@
+// const userEmail = false
+
+// if(userEmail){
+//     console.log("Got User Email")
+
+// }else {
+//     console.log("Don't have user Email")
+// }
+
+// falsy values
+// false, 0 , -0 , BigInt 0n, null , undefined , NaN
+
+// truthy values
+// "0", "false", " ", [], {}, function(){}
+
+// **************Nullish Coalescing Operator (??): null, undefined****************
+
+// let val1;
+// // val1 = 5 ?? 10
+// // val1 = null ?? 10
+// // val1 = undefined ?? 15
+// // val1 = null ?? 15 ?? 20
+// val1 = null ?? undefined
+
+// console.log(val1);
+
+// ******************************Ternary Operator*********************************************
+
+const chai = "milk wali chai"
+chai ==="paani wali chai" ? console.log("ha hai"): console.log("nahi hai")
